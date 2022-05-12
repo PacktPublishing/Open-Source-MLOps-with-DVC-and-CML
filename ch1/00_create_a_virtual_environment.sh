4383aba707ca7e7c966c9cd9cf1b27ce77854472
Matthew Upson <matt@mantisnlp.com>
2022-05-12 22:48:45 +0200

    new: Add ch1/00_create_a_virtual_environment.sh

---
+++ ch1/00_create_a_virtual_environment.sh
@@ -0,0 +1,34 @@
+# Define the project folder we will use for Chapter 1
+
+PROJECT_PATH=~/Documents/my_project
+
+# Make the folder
+
+mkdir $PROJECT_PATH
+cd $PROJECT_PATH
+
+# Initialise a git repository
+
+git init
+
+# Create a virtual environment using python venv and activate it
+
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dvc and initialize a dvc repository
+
+pip install dvc
+dvc init
+
+# Add a .gitignore
+
+echo venv > .gitignore
+
+# Create a requirements.txt file
+
+pip freeze > requirements.txt
+
+# Make the first git commit
+
+git commit -am 'Initial commit'
