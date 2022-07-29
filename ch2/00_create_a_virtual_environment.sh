cad7167db208ca31765865914072a72d7fffe8e2
ivyleavedtoadflax <matt@mantisnlp.com>
2022-07-29 18:23:37 +0200

    Rename chapters to accomodate new itroductory chapter (#5)
    * new: Add ch1/00_create_a_virtual_environment.sh
    
    * new: Add ch1/01_adding_files_to_dvc.sh
    
    * new: Add ch1 data
    
    * chg: Update ch1 data
    
    * new: Add ch1/02_tracking_changes_to_data.sh
    
    * Also updates 00, and 01.
    
    * new: Add ch2
    
    * new: Add ch3
    
    * Rename files

---
+++ ch2/00_create_a_virtual_environment.sh
@@ -0,0 +1,36 @@
+# Define the project folder we will use for Chapter 1
+
+PROJECT_PATH=~/Documents/mantis/books/os-mlops-dvc
+
+# Make the folder
+
+mkdir $PROJECT_PATH
+cd $PROJECT_PATH
+
+# Initialise a git repository
+
+git init
+git checkout -b main
+
+# Create a virtual environment using python venv and activate it
+
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dvc and initialize a dvc repository
+
+pip install dvc
+dvc init
+
+# Add a .gitignore
+
+echo venv > .gitignore
+
+# Create a requirements.txt file
+
+pip freeze > requirements.txt
+
+# Make the first git commit
+
+git add .gitignore requirements.txt
+git commit -am 'Initial commit'
---
+++ ch2/01_adding_files_to_dvc.sh
@@ -0,0 +1,34 @@
+
+cd $PROJECT_PATH
+
+# Download the zipped data from github using curl. You can also use another
+# tool or follow the link to do this manually.
+
+curl -L https://github.com/PacktPublishing/Open-Source-MLOPs-with-DVC-and-CML/blob/ce1fd2e5205b92d8c16e449e3fb2a67f380b6dde/ch1/IMDB_movie_ratings_sentiment.zip?raw=true > $PROJECT_PATH/IMDB_movie_ratings_sentiment.zip
+
+# Make a new folder in which to store the data
+
+mkdir -p data
+
+# Unzip the data and move into the data folder
+
+unzip IMDB_movie_ratings_sentiment.zip
+mv IMDB_movie_ratings_sentiment.csv data
+
+# Add the data file to DVC
+
+dvc add data/IMDB_movie_ratings_sentiment.csv
+
+# Add the newly created files to DVC. These are the .dvc file which tracks the
+# file's hash digest, and a folder specific .gitignore file which will force git
+# to ignore the data file.
+
+git add data/IMDB_movie_ratings_sentiment.csv.dvc data/.gitignore
+
+# Let's commit both of these new files to Git
+
+git commit -m "Add raw data to dvc"
+
+# Finally we delete the zip file, which we no longer need.
+
+rm $PROJECT_PATH/IMDB_movie_ratings_sentiment.zip
