e616ee68820edd1286f37b749f81080cd6788d41
Matthew Upson <matt@mantisnlp.com>
2022-08-10 09:49:45 +0100

    ch06 02 Add process_data stage to the pipeline

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Open-Source-MLOPs-with-DVC-and-CML
+
+## DAG
+
+```mermaid
+flowchart TD
+	node1["data/IMDB_movie_ratings_sentiment.csv"]
+	node2["data/processed/X_test.txt"]
+	node3["data/processed/X_train.txt"]
+	node4["data/processed/y_test.txt"]
+	node5["data/processed/y_train.txt"]
+	node1-->node2
+	node1-->node3
+	node1-->node4
+	node1-->node5
+```
---
+++ process_data.py
@@ -0,0 +1,52 @@
+
+"""
+Simple script to create train and test splits
+"""
+
+import csv
+import os
+
+from sklearn.model_selection import train_test_split
+
+
+def prepare_data(data_path: str, test_size: float):
+    """
+    Loads the data from the csv file and split it into train and test sets.
+
+    Args:
+        data_path: path to the csv file containing the data.
+        test_size: proportion of the data to be used for testing.
+    """
+    data = []
+    print(f"Loading data from {data_path}")
+    with open(data_path, encoding="utf-8-sig") as f:
+        csvreader = csv.DictReader(f)
+        for row in csvreader:
+            data.append((row["text"], row["label"]))
+    X, y = zip(*data)
+
+    print(f"Loaded {len(X)} examples")
+    print("Splitting data into train and test sets")
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+
+    print(f"Train set size: {len(X_train)}")
+    print(f"Test set size: {len(X_test)}")
+
+    return X_train, X_test, y_train, y_test
+
+def save_data(output_path: str, **kwargs):
+    """
+    Saves train and test splits to disk
+    """
+    os.makedirs(output_path, exist_ok=True)
+    for k, v in kwargs.items():
+        output_file = os.path.join(output_path, f"{k}.txt")
+        print(f"Saving {k} to {output_file}")
+        with open(output_file, "w") as f:
+            for item in v:
+                f.write(item + "\n")
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = prepare_data(data_path="data/IMDB_movie_ratings_sentiment.csv", test_size=0.2)
+    save_data(output_path="data/processed/", X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
