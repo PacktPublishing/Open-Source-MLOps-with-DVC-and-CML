1b8ff7c0c04c2fd4a4c65a3e002bd7aee61a4f8a
Matthew Upson <matt@mantisnlp.com>
2022-10-30 16:25:59 +0100

    ch10/02 Fetch model from remote cache

--- api.py
+++ api.py
@@ -1,19 +1,31 @@
+import os
+
 import joblib
+from dvc.api import DVCFileSystem
 from fastapi import FastAPI
 
-# Load the model from disk
+REV = os.getenv("REV", "main")
+REMOTE = os.getenv("REMOTE", "s3")
+LOCAL_PATH = os.getenv("LOCAL_PATH", "/tmp/model.joblib")
+REMOTE_PATH = os.getenv("REMOTE_PATH", "models/model.joblib")
+REPO = os.getenv(
+    "REPO", "https://github.com/PacktPublishing/Open-Source-MLOps-with-DVC-and-CML.git"
+)
 
-model = joblib.load("models/model.joblib")
+# Load model artefacts from cache
 
-# Initialize the app
+fs = DVCFileSystem(REPO, rev=REV)
+fs.get_file(REMOTE_PATH, LOCAL_PATH, remote=REMOTE)
+
+print(f"Loading model from {LOCAL_PATH}")
+model = joblib.load(LOCAL_PATH)
 
 app = FastAPI()
-
-# Add a predict endpoint
 
 
 @app.post("/predict")
 def predict(data: dict):
     pred = model.predict([data["text"]])
     pred = pred[0].split("\n")[0]
+
     return {"class": pred}
